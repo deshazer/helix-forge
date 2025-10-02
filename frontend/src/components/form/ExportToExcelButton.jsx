c122feef8a049c86cb5882efa4d348093873264a
Steven Deshazer <stevendeshazer@tukios.com>
2025-10-02 02:36:21 -0500

    Added price history exports to excel

--- frontend/src/components/form/ExportToExcelButton.jsx
+++ frontend/src/components/form/ExportToExcelButton.jsx
@@ -1,24 +1,44 @@
-import { exportToExcel } from '@/lib/utils'
+import { exportToExcel, getErrorMessage } from '@/lib/utils'
 import { FileDown } from 'lucide-react'
 import { Button } from '../ui/button'
+import { toast } from 'sonner'
 
 const ExportToExcelButton = ({
   children,
   className,
+  loader,
   icon = true,
   data = [],
   filename = '',
   ...props
 }) => {
-  if (!data || data.length === 0) {
+  if ((!data || data.length === 0) && !loader) {
     return null
   }
   return (
     <Button
       className={className}
       variant="outline"
-      onClick={() => {
-        exportToExcel(data, filename)
+      onClick={async () => {
+        if (loader && typeof loader === 'function') {
+          try {
+            const loadedData = await Promise.resolve(loader())
+            if (!loadedData || loadedData.length === 0) {
+              throw new Error('No data found.')
+            }
+            if (!Array.isArray(loadedData)) {
+              throw new Error('Price history is not correctly formatted.')
+            }
+            return exportToExcel(loadedData, filename)
+          } catch (error) {
+            console.error(error)
+            toast.error('Error loading data.', {
+              description: getErrorMessage(error),
+            })
+          }
+        } else {
+          exportToExcel(data, filename)
+        }
       }}
       {...props}
     >
--- frontend/src/routes/_authenticated/reports/quotes.jsx
+++ frontend/src/routes/_authenticated/reports/quotes.jsx
@@ -3,10 +3,17 @@ import SearchInput from '@/components/form/SearchInput'
 import StockDisplay from '@/components/StockDisplay'
 import { Button } from '@/components/ui/button'
 import LoadingSpinner from '@/components/ui/loading-spinner'
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select'
 import Txt from '@/components/ui/typography'
 import useToastQueryError from '@/hooks/useToastQueryError'
 import { quoteQueries } from '@/lib/quotes/quotes.query'
-import { useQuery } from '@tanstack/react-query'
+import { useQuery, useQueryClient } from '@tanstack/react-query'
 import { createFileRoute, useNavigate } from '@tanstack/react-router'
 import dayjs from 'dayjs'
 import timezone from 'dayjs/plugin/timezone.js'
@@ -39,9 +46,10 @@ export const Route = createFileRoute('/_authenticated/reports/quotes')({
 
 function QuotesComponent() {
   const navigate = useNavigate({ from: Route.fullPath })
+  const queryClient = useQueryClient()
   const { symbol } = Route.useSearch()
   const [search, setSearch] = useState(symbol || '')
-  const [results, setResults] = useState(null)
+  const [period, setPeriod] = useState('1d')
   const {
     data: quote,
     refetch,
@@ -60,7 +68,7 @@ function QuotesComponent() {
 
   useToastQueryError(error, 'Error getting quote')
 
-  const handleSubmit = async (e) => {
+  const handleSearchSymbol = async (e) => {
     e.preventDefault()
     e.stopPropagation()
 
@@ -71,11 +79,20 @@ function QuotesComponent() {
     refetch()
   }
 
+  const handleExportPriceHistory = async () => {
+    const priceHistory = await queryClient.fetchQuery(
+      quoteQueries.priceHistory(symbol, { price_history: period }),
+    )
+    console.log('priceHistory', priceHistory)
+
+    return priceHistory?.candles
+  }
+
   return (
     <div className="flex flex-col space-y-4">
       <Txt.h3>Quotes</Txt.h3>
       <form
-        onSubmit={handleSubmit}
+        onSubmit={handleSearchSymbol}
         className="inline-flex items-center gap-x-2"
       >
         <SearchInput
@@ -90,11 +107,25 @@ function QuotesComponent() {
         </Button>
       </form>
 
-      <StockDisplay data={quote} symbol={search.toUpperCase()} />
+      <StockDisplay data={quote} symbol={search} />
 
       {!!quote && (
-        <div>
-          <ExportToExcelButton data={quote?.price_history?.candles}>
+        <div className="inline-flex items-center gap-x-4 gap-y-2 flex-wrap">
+          <Select value={period} onValueChange={setPeriod}>
+            <SelectTrigger>
+              <SelectValue placeholder="Select sample rate" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="1m">Every 1m</SelectItem>
+              <SelectItem value="5m">Every 5m</SelectItem>
+              <SelectItem value="10m">Every 10m</SelectItem>
+              <SelectItem value="15m">Every 15m</SelectItem>
+              <SelectItem value="30m">Every 30m</SelectItem>
+              <SelectItem value="1d">Daily</SelectItem>
+              <SelectItem value="1w">Weekly</SelectItem>
+            </SelectContent>
+          </Select>
+          <ExportToExcelButton loader={handleExportPriceHistory}>
             Export Price History
           </ExportToExcelButton>
         </div>
